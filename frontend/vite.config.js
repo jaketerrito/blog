9c4866a9c5757739f705764b2934ca39c360ae32
jaketerrito <jaketerrito@gmail.com>
2025-05-08 21:21:25 -0400

    Dev conatiners can communicate

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
   ],
+  server: {
+    allowedHosts: ['*']
+  },
 })
