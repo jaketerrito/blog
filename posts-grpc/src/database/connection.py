06e192b080038a277cc4a8f157788c648a3e47b0
jaketerrito <jaketerrito@gmail.com>
2025-09-07 21:13:20 -0400

    Mogno connection via seperate env variables

--- posts-grpc/src/database/connection.py
+++ posts-grpc/src/database/connection.py
@@ -3,4 +3,5 @@ import os
 
 
 def init_db_connection():
-    connect(host=os.getenv("DB_URI"))
+
+    connect(host=os.getenv("MONGO_HOST"), port=int(os.getenv("MONGO_PORT")), username=os.getenv("MONGO_USER"), password=os.getenv("MONGO_PASSWORD"))
