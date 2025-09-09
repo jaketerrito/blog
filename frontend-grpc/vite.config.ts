9b163e138d8f17a8dde45a2d268ed0b536699255
jaketerrito <jaketerrito@gmail.com>
2025-09-08 22:48:31 -0400

    Produ server setttings

--- frontend-grpc/vite.config.ts
+++ frontend-grpc/vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
   },
   plugins: [
     tsConfigPaths(),
-    tanstackStart({ customViteReactPlugin: true }),
+    tanstackStart({ 
+      customViteReactPlugin: true,
+      target: 'node-server' 
+    }),
     viteReact(),
   ],
 });
