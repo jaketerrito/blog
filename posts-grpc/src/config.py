b155b0a54a8aa0667dc325df805ea3fa6075ff4b
jaketerrito <jaketerrito@gmail.com>
2025-09-10 19:51:15 -0400

    Sipmler mogno

--- posts-grpc/src/config.py
+++ posts-grpc/src/config.py
@@ -1,5 +1,6 @@
 import os
 
+MONGO_DB = os.getenv("MONGO_DB")
 MONGO_HOST = os.getenv("MONGO_HOST")
 MONGO_PORT = int(os.getenv("MONGO_PORT"))
 MONGO_USER = os.getenv("MONGO_USER")
--- posts-grpc/src/database/connection.py
+++ posts-grpc/src/database/connection.py
@@ -4,9 +4,9 @@ from src import config
 
 def init_db_connection():
     connect(
+        db=config.MONGO_DB,
         host=config.MONGO_HOST,
         port=int(config.MONGO_PORT),
         username=config.MONGO_USER,
         password=config.MONGO_PASSWORD,
-        authMechanism="SCRAM-SHA-256",
     )
